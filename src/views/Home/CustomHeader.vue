a95ff47db43aa4582cdc9742b95203c231a0821d
Victor Matheus <victormatheus507@gmail.com>
2024-07-18 11:10:46 -0300

    criando tela inicial 2

--- src/views/Home/CustomHeader.vue
+++ src/views/Home/CustomHeader.vue
@@ -1,7 +1,72 @@
 <template>
-  <h1>Meu header</h1>
+  <header class="header">
+    <div class="header-group">
+      <div class="flex items-center justify-between py-10">
+        <div class="w-28 lg:w-36">
+          <img
+            class="w-full"
+            src="../../assets/images/logo_white.png"
+            alt="logo"
+          />
+        </div>
+
+        <div class="flex">
+          <button
+            class="px-6 py-2 font-bold rounded-full text-white focus:outlin"
+          >
+            Crie uma conta
+          </button>
+          <button
+            class="px-6 py-2 font-bold bg-white rounded-full text-brand-main focus:outlin"
+          >
+            Entrar
+          </button>
+        </div>
+      </div>
+
+      <div class="flex flex-col mt-28 space-y-2">
+        <h1 class="text-4xl font-black text-white">
+          Tenha um feedback. <br />
+          E faça seus clientes mais <br class="hidden lg:inline-block" />
+          felizes!
+        </h1>
+        <p class="text-lg font-medium text-gray-200">
+          Receba ideias, reclamações e feedbacks com um
+          <br class="hidden lg:inline-block" />
+          simples widget na página.
+        </p>
+        <div>
+          <button
+            class="px-6 py-2 font-bold bg-white rounded-full text-brand-main focus:outlin mt-9"
+          >
+            Crie uma conta gratis
+          </button>
+        </div>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
 export default {};
 </script>
+
+<style lang="postcss" scoped>
+.header {
+  @apply bg-brand-main w-full flex justify-center;
+  height: 700px;
+}
+
+.header-group {
+  @apply flex flex-col w-4/5 max-w-6xl;
+}
+
+@media (min-width: 640px) {
+  .header-group {
+    background-image: url(../../assets/images/blue_balloons.png);
+    background-size: 628px;
+    background-position: 90% 100%;
+    background-repeat: no-repeat;
+  }
+}
+</style>
